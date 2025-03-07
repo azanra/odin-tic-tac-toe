8e2e9c5dd60ab2832c9a9fb19ffa74a3a4d4664e
azan-personal <112457939+azanra@users.noreply.github.com>
2025-03-08 05:07:31 +0700

    Add method to place a player mark

--- script/gameBoard.js
+++ script/gameBoard.js
@@ -19,10 +19,23 @@ export const gameBoard = (function () {
 
   const decrementEmptyCell = () => emptyCell--;
 
+  const placeMark = (mark, inputRow, inputCol) => {
+    for (let i = 0; i < row; i++) {
+      for (let j = 0; j < col; j++) {
+        if (i === inputRow && j === inputCol) {
+          gameBoard[i][j] = mark;
+          decrementEmptyCell();
+          console.log(returnEmptyCell());
+        }
+      }
+    }
+  };
+
   return {
     createGameBoard,
     returnGameBoard,
     returnEmptyCell,
     decrementEmptyCell,
+    placeMark,
   };
 })();
