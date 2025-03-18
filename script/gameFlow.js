f50c446250df808e51df6d4a28a14a547f373d02
azan-personal <112457939+azanra@users.noreply.github.com>
2025-03-19 05:51:48 +0700

    Check if player win the game and reset all object

--- script/gameFlow.js
+++ script/gameFlow.js
@@ -1,9 +1,39 @@
+import { gameBoard } from "./gameBoard.js";
+import { player } from "./player.js";
+import { view } from "./view.js";
+
 export const gameFlow = (function () {
   let winStatus = false;
 
   const returnWinStatus = () => winStatus;
 
+  const resetWinStatus = () => (winStatus = false);
+
   const updateWinStatus = () => (winStatus = !winStatus);
 
-  return { returnWinStatus, updateWinStatus };
+  const playRound = () => {
+    gameBoard.createGameBoard();
+    view.createColumn();
+    view.domListener();
+    view.displayDom();
+  };
+
+  const checkWin = () => {
+    if (winStatus === true) {
+      const paraStatus = document.querySelector(".winStatus");
+      paraStatus.textContent = `${view.returnCheckPlayer()} is winning the game`;
+      reset();
+    }
+  };
+
+  const reset = () => {
+    gameBoard.resetGameBoard();
+    view.resetDomBoard();
+    player.resetActivePlayer();
+    resetWinStatus();
+    gameBoard.createGameBoard();
+    view.displayDom();
+  };
+
+  return { returnWinStatus, updateWinStatus, playRound, checkWin };
 })();
--- script/script.js
+++ script/script.js
@@ -1,7 +1,3 @@
-import { gameBoard } from "./gameBoard.js";
-import { player } from "./player.js";
 import { gameFlow } from "./gameFlow.js";
-import { view } from "./view.js";
 
 gameFlow.playRound();
-view.createColumn();
